63c81f7e936c54e88c4d81adabbdceddcb3269aa
bpieczek <bartosz.pieczek@pisz.to>
2022-11-12 17:23:09 +0100

    Add checking if car is avaliable

--- backend/controllers/api/RentingController.js
+++ backend/controllers/api/RentingController.js
@@ -1,4 +1,6 @@
+const schedule = require("node-schedule");
 const Renting = require("../../database/models/renting");
+const Cars = require("../../database/models/car");
 
 class RentingController {
   async rentCar(req, res) {
@@ -9,25 +11,43 @@ class RentingController {
 
     let renting;
     let doc;
+    let cars;
+    let isAvaliable = true;
+
     try {
-      /*doc = await Renting.find({});
-      doc.forEach((renting) => {
+      doc = await Renting.find({});
+      cars = await Cars.find({});
+
+      cars.forEach((car) => {
         if (
-          renting.whichCar === whichCar &&
-          ((renting.from.getTime() <= from.getTime() &&
-            renting.to.getTime() >= from.getTime()) ||
-            (renting.from.getTime() <= to.getTime() &&
-              renting.to.getTime() >= to.getTime()))
+          car.howManyCars === 1 &&
+          whichCar === `${car.brand} (${car.category})`
         ) {
-          return res.status(422).json({ message: "Car is not available in this period!" });
+          doc.forEach((renting) => {
+            if (
+              renting.whichCar === whichCar &&
+              ((renting.from.getTime() <= from.getTime() &&
+                renting.to.getTime() >= from.getTime()) ||
+                (renting.from.getTime() <= to.getTime() &&
+                  renting.to.getTime() >= to.getTime()))
+            ) {
+              isAvaliable = false;
+              return res.status(422).json({
+                message: `Car ${whichCar} is not available in this period!`,
+              });
+            }
+          });
         }
-      });*/
-      renting = new Renting({ whichCar, from, to, whosRenting });
-      await renting.save();
+      });
+      if (isAvaliable) {
+        renting = new Renting({ whichCar, from, to, whosRenting });
+        await renting.save();
+      } else {
+        return;
+      }
     } catch (err) {
       return res.status(422).json({ message: err.message });
     }
-
     return res.status(201).json(renting);
   }
 }
