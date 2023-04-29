b087e7f96e8775a7823903069f5d8dc7d0ca3264
bpieczek <bartosz.pieczek@pisz.to>
2023-04-29 20:02:38 +0200

    Axios update

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.API_URL || "rent-car-backend-yzto.onrender.com/api",
+  baseURL: process.env.API_URL || "https://rent-car-backend-yzto.onrender.com/api",
 });
 
 export default instance;
